a0a2f2db81fc9c52adfa7d110bd4b51f7a042ee8
Prachi Varshney <86619102+prachi-varshney@users.noreply.github.com>
2025-01-17 18:20:06 +0530

    view.php

--- Project/view.php
+++ Project/view.php
@@ -31,7 +31,14 @@
 
 
 
+CREATE DATABASE Project
 
+USE Project
+projectCREATE TABLE Project (project
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
 
    
 </body>
